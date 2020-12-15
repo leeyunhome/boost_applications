9be3cf87abacf9d8be8f23d50fdd3f0c95fee666
leeyunhome <leeyunhome@daum.net>
2020-12-16 04:55:29 +0900

    add "없는 옵션은 구성 파일에서 읽어오기"

--- Chapter01/first_program/first_program.cpp
+++ Chapter01/first_program/first_program.cpp
@@ -10,11 +10,11 @@ int main(int argc, char *argv[])
 {
     opt::options_description desc("All options");
 
-    desc.add_options()
-        ("apples", opt::value<int>(), "how many apples do you have")
-        ("oranges", opt::value<int>(), "how many oranges do you have")
-        ("help", "produce help message")
-        ;
+    //desc.add_options()
+    //    ("apples", opt::value<int>(), "how many apples do you have")
+    //    ("oranges", opt::value<int>(), "how many oranges do you have")
+    //    ("help", "produce help message")
+    //    ;
     
     int oranges_var = 0;
     desc.add_options()
@@ -34,12 +34,40 @@ int main(int argc, char *argv[])
     // 'opt::store with' 'parse_environment' 메소드를 쓰면 된다.
 
     // 파싱 후 저장한 다음에 불러야 한다.
-    opt::notify(vm);
+    //opt::notify(vm);
 
     if (vm.count("help"))
     {
         std::cout << desc << "\n";
         return 1;
+    }
+
+    // "apples_oranges.cfg" 구성 파일에서 빠진 옵션을 추가한다.
+    try
+    {
+        opt::store(
+            opt::parse_config_file<char>("apples_oranges.cfg", desc),
+            vm
+        );
+    }
+    catch (const opt::reading_file& e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        // 필수 옵션이 빠져있다면 `opt::required_option` 예외를 던진다.
+        opt::notify(vm);
+    }
+    catch (const opt::required_option& e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+        return 2;
+    }
+
+    if (vm.count("name")) {
+        std::cout << "Hi," << vm["name"].as<std::string>() << "!\n";
     }
 
     std::cout << "Fruits count: "
